06f962e27c01fe9dd665753e2de04992de29f534
ahacad <ahacad@126.com>
2021-09-23 23:41:09 +0800

    chore: improve data store

--- src/store/dataSlice.ts
+++ src/store/dataSlice.ts
@@ -1,15 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+function getTodayString(): string {
+  const date = new Date();
+  const [day, month, year] = [
+    date.getDate(),
+    date.getMonth(),
+    date.getFullYear(),
+  ];
+
+  return String(year) + "-" + String(month) + "-" + String(day);
+}
+
 export const dataSlice = createSlice({
   name: "data",
-  initialState: { value: 0 },
+  initialState: { days: {} },
   reducers: {
-    increment: (state) => {
-      state.value += 1;
+    increment: (state, action) => {
+      //
+    },
+    newclock: (state, action) => {
+      const today: string = getTodayString();
+      if (state.days[today]) {
+        state.days[today].push(action.payload);
+      } else {
+        state.days[today] = [action.payload];
+      }
+        // TODO: save data to local file
     },
   },
 });
 
-export const { increment } = dataSlice.actions;
+export const { newclock } = dataSlice.actions;
 
 export default dataSlice.reducer;
