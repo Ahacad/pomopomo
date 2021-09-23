29a5c08c299ffbd4691ed8427c2cd53509a8b6b3
ahacad <ahacad@126.com>
2021-09-23 22:31:02 +0800

    chore: remove redundant code, change bg color

--- src/App.tsx
+++ src/App.tsx
@@ -16,41 +16,14 @@ function App() {
     setText(j.somethin);
   };
 
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Pomopomo!</p>
         <button onClick={handleClick}>a</button>
         <Clock />
         <p>this is: {text}</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
       </header>
     </div>
   );
