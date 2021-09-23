a1d54d37ea4c9f5d67bf63a6fc4dd38ad872c5f7
ahacad <ahacad@126.com>
2021-09-23 22:31:21 +0800

    feat: basic clock

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -1,11 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Clock() {
+  const [alltime, setAlltime] = useState(1500);
+  const [timenow, setTimenow] = useState(1500);
+  const [clockrunning, setClockrunning] = useState(false);
+
+    function clockStart() { setClockrunning(true); }
+
+    function clockPause() { setClockrunning(false); }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (!clockrunning) return;
+      setTimenow(timenow - 1);
+    }, 1000);
+    return () => clearTimeout(timer);
+  });
+
   return (
-    <div>
-      <button>start</button>
-      <button>pause</button>
-      <button>jump</button>
+    <div className="">
+      <div>{timenow}</div>
+      <button className="bg-white text-black mr-2" onClick={clockStart}>
+        start
+      </button>
+      <button className="bg-white text-black mr-2" onClick={clockPause}>pause</button>
+      <button className="bg-white text-black mr-2">jump</button>
     </div>
   );
 }
