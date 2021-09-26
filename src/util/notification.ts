6f8d655709e4e78a0f68c7ec4d60704dab79c2e3
ahacad <ahacad@126.com>
2021-09-26 21:44:13 +0800

    chore(util): new utility folder: notification

---
+++ src/util/notification.ts
@@ -0,0 +1,13 @@
+export function notify(title: string, options: NotificationOptions) {
+  if (!("Notification" in window)) {
+    console.warn("Notification not supported in this browser");
+  } else if (Notification.permission === "granted") {
+    let notification = new Notification(title, options);
+  } else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        let notification = new Notification(title, options);
+      }
+    });
+  }
+}
