e2e8edaac3da5c4dc09672471e640bbd2cfbea85
ahacad <ahacad@126.com>
2021-09-26 21:34:27 +0800

    chore(store): save data to localstorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit';
-import dataReducer from './dataSlice';
-import clockReducer from './clockSlice';
+import { configureStore } from "@reduxjs/toolkit";
 
-export default configureStore({
-    reducer: {
-        data: dataReducer,
-        clock: clockReducer,
-    }
-})
+import clockReducer from "./clockSlice";
+import dataReducer from "./dataSlice";
+
+type RootState = ReturnType<typeof store.getState>;
+
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (!serializedState) return undefined;
+    else return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+export const saveState = (state: RootState) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const store = configureStore({
+  reducer: {
+    data: dataReducer,
+    clock: clockReducer,
+  },
+  preloadedState: loadState(),
+});
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
+export default store;
