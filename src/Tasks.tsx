0cdb34ac09b817e63a0b8f0557459029e07b8987
ahacad <ahacad@126.com>
2021-09-29 21:50:46 +0800

    fix(tasks): using state to manage selection

--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -1,16 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { newclock } from "./store/dataSlice";
 import { useSelector, useDispatch } from "react-redux";
-import { stop, start, reset, decrease } from "./store/clockSlice";
-import { notify } from "./util";
+import { selectTask } from "./store/dataSlice";
 
 import { RiCheckboxCircleLine } from "react-icons/ri";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { Task as taskType } from "./types";
 
 function Task({ data }: { data: taskType }) {
+  const dispatch = useDispatch();
+  const selectedTask = useSelector((state) => state.data.selectedTask);
+  function toggleSelect() {
+    if (selectedTask === data.id) {
+      dispatch(selectTask(0));
+    } else {
+      dispatch(selectTask(data.id));
+    }
+  }
+  function isSelected(): string {
+    if (selectedTask === data.id) {
+      return "border-indigo-400 transform translate-y-0.5";
+    }
+    return "";
+  }
+
   return (
-      <button className="bg-white w-full text-black flex justify-between p-4 mb-2 rounded-md border-l-8 hover:border-indigo-400 focus:border-indigo-400 transform focus:translate-y-0.5">
+    <button
+      onClick={toggleSelect}
+      className={`bg-white w-full text-black flex justify-between p-4 mb-2 rounded-md border-l-8 hover:border-indigo-400 transform ${isSelected()}`}
+    >
       <div className="flex">
         <RiCheckboxCircleLine className="mt-2 mr-1" />
         {data.name}
