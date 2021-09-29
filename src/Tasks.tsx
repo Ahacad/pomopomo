180e4c312d285760967e66e56a4176b7a405fd4b
ahacad <ahacad@126.com>
2021-09-29 17:09:22 +0800

    chore(tasks): add styles

--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -5,18 +5,20 @@ import { stop, start, reset, decrease } from "./store/clockSlice";
 import { notify } from "./util";
 
 import { RiCheckboxCircleLine } from "react-icons/ri";
+import { BsThreeDotsVertical } from "react-icons/bs";
 import { Task as taskType } from "./types";
 
 function Task({ data }: { data: taskType }) {
   return (
-    <div className="bg-white w-full text-black flex justify-between">
+    <div className="bg-white w-full text-black flex justify-between p-4 mb-2 rounded-md">
       <div className="flex">
         <RiCheckboxCircleLine />
         {data.name}
       </div>
-      <div>
+      <div className="flex">
         {data.finishedPomodoro} /{" "}
         {data.estimationPomodoro ? data.estimationPomodoro : 0}
+        <BsThreeDotsVertical />
       </div>
     </div>
   );
@@ -27,7 +29,7 @@ export default function Tasks() {
 
   return (
     <div className="">
-      <div className="w-96">
+      <div className="w-96 mt-10">
         {tasks.map((task: taskType) => (
           <Task data={task} />
         ))}
