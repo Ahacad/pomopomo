dd5d62f30282ba45720f41bec8cabf2a78e8c951
ahacad <ahacad@126.com>
2021-09-29 16:38:28 +0800

    chore(tasks): task single component

--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -3,18 +3,27 @@ import { newclock } from "./store/dataSlice";
 import { useSelector, useDispatch } from "react-redux";
 import { stop, start, reset, decrease } from "./store/clockSlice";
 import { notify } from "./util";
-import ClockOptions from "./components/ClockOptions";
+
+import { RiCheckboxCircleLine } from "react-icons/ri";
 
 function Task() {
-  return <div></div>;
+  return (
+    <div className="bg-white w-full text-black flex">
+      <div className="flex">
+        <RiCheckboxCircleLine />
+        TASK
+      </div>
+    </div>
+  );
 }
 
 export default function Tasks() {
   return (
     <div className="">
-      <div>
-        <div className="bg-white w-36 text-black">tasks1</div>
-        <div className="bg-white w-36 text-black">task2</div>
+      <div className="w-96">
+        <Task />
+        <Task />
+        <Task />
       </div>
     </div>
   );
